69d87671ae6c9fae428494ad5a343cfe5b37694d
lavender-aa <sodaissoda@gmail.com>
2026-01-20 18:30:15 -0500

    added event and thought generation commands
    commands paste in the current time as appropriate, adding any necessary spaces for padding

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
 import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
 
-// Remember to rename these classes and interfaces!
+// behaviors i want:
 
-export default class MyPlugin extends Plugin {
+// command AddEvent with: "- (H:MM(a|p)) " based on current time
+// when entering a newline from an event, create another one with the current time
+
+// command AddThought with: "(H:MM(a|p))"
+
+// for thoughts: at first only first line appears, but wraps around as follows:
+
+// (H:MMp) text text text text text text text
+//       | uses a bar whenever text wraps over
+//       |
+//       | or whenever the enter key is pressed.
+//       | hitting backspace on an empty line,
+//       | like the one below:
+//       |
+//       | will delete that line and put the cursor at the beginning of the line.
+
+// this part should act very similar to a bulleted list
+
+
+
+export default class JournalFormats extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
@@ -19,51 +39,27 @@ export default class MyPlugin extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status bar text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
+		// command: AddEvent
+		this.addCommand({
+			id: 'add-event',
+			name: 'Add event template',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				editor.replaceSelection('- ' + getTimestamp());
 			}
-		});
+		})
+		
+		// command: AddThought
+		this.addCommand({
+			id: 'add-thought',
+			name: 'Add thought template',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				editor.replaceSelection(getTimestamp();
+			}
+		})
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -82,18 +78,9 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+function getTimestamp(): string {
+		let date: Date = new Date();
+		let str: string = date.toLocaleString();
+		let time: string = str.slice(11, 15) + str.charAt(19).toLowerCase();
+		return (time.length == 5 ? ' ' : '') + '(' + time + ') ';
 	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
